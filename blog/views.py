6d1568a7e4ec5c9f7dd6aa17fb2786f5a959a9d2
vicenteyah <vicente.yah.dzul@icloud.com>
2022-05-28 14:02:35 -0500

    views configured as ModelViewSet

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 # Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def retrieve(self, request, pk=None):
+        queryset = Post.objects.all()
+        post = get_object_or_404(queryset, pk=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
