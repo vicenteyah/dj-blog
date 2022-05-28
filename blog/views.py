1f7d3823163f003116ca3013549d042e17f10568
vicenteyah <vicente.yah.dzul@icloud.com>
2022-05-28 15:36:02 -0500

    custom endpoind added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 # Create your views here.
 from .models import Post
@@ -10,10 +11,10 @@ from .serializers import PostSerializer
 class BlogViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-    def retrieve(self, request, pk=None):
+        
+    @action(detail=True, methods=['get'])
+    def get_one_by_id(self, request, pk=None):
         queryset = Post.objects.all()
         post = get_object_or_404(queryset, pk=pk)
         serializer = PostSerializer(post)
         return Response(serializer.data)
-
